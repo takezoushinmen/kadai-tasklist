fee911e7ee111cb4afaea8e16c91bed8f07c2e65
Takezou.Shinmen <ec2-user@ip-172-31-17-29.us-east-2.compute.internal>
2018-05-30 07:22:07 +0000

    validation　modified

--- resources/views/tasklists/show.blade.php
+++ resources/views/tasklists/show.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>id = {{ $tasklist->id }} のタスクリスト詳細ページ</h1>
-        <p>タスク: {{ $tasklist->content }}</p>
-        <p>ステータス: {{ $tasklist->status }}</p
+<h1>id = {{ $tasklist->id }} のタスク詳細ページ</h1>
+
+    <p>{{ $tasklist->content }}</p>
+    
+    <p>ステータス: {{ $tasklist->status }}</p>
     
     {!! link_to_route('tasklists.edit', 'このタスクを編集', ['id' => $tasklist->id]) !!}
-        {!! Form::model($tasklist, ['route' => ['tasklists.destroy', $tasklist->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+    
+    {!! Form::model($tasklist, ['route' => ['tasklists.destroy', $tasklist->id], 'method' => 'delete']) !!}
+    {!! Form::submit('削除') !!}
     {!! Form::close() !!}
 
 @endsection
