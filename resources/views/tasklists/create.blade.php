44a103d7e11c1c719c433d7931957fd52b4394a3
Takezou.Shinmen <ec2-user@ip-172-31-17-29.us-east-2.compute.internal>
2018-06-04 03:24:11 +0000

    bootstrap

--- resources/views/tasklists/create.blade.php
+++ resources/views/tasklists/create.blade.php
@@ -3,17 +3,21 @@
 @section('content')
 
     <h1>タスク新規作成ページ</h1>
-    
-
-    {!! Form::model($tasklist, ['route' => 'tasklists.store']) !!}
-
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        <div class="row">
+        <div class="col-xs-6">
+            {!! Form::model($tasklist, ['route' => 'tasklists.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
         
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-        
-        {!! Form::submit('投稿') !!}
-
-    {!! Form::close() !!}
+            {!! Form::close() !!}
 @endsection
--- resources/views/tasklists/edit.blade.php
+++ resources/views/tasklists/edit.blade.php
@@ -3,16 +3,21 @@
 @section('content')
 
     <h1>id: {{ $tasklist->id }} のタスク編集ページ</h1>
-
+    
+    <div class="row">
+        <div class="col-xs-6">
     {!! Form::model($tasklist, ['route' => ['tasklists.update', $tasklist->id], 'method' => 'put']) !!}
-
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        <div class="form-group">    
+            {!! Form::label('content', 'タスク:') !!}
+            {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
         
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+        <div class="form-group">
+            {!! Form::label('status', 'ステータス:') !!}
+            {!! Form::text('status', null, ['class' => 'form-control']) !!}
+        </div>
         
-        {!! Form::submit('更新') !!}
+        {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
 
     {!! Form::close() !!}
 
